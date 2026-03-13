4eb0a718b6c14d92aa97669a3a02e26f9a99dfbc
Muneeb <27100022@lums.edu.pk>
2026-03-14 03:49:03 +0500

    Lab Participation Exercise - DeleteCity failing test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -12,4 +12,13 @@ public class CustomListTest {
         list.addCity(calgary);
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City city = new City("Edmonton", "AB");
+        list.addCity(city);
+        list.deleteCity(city);
+        assertFalse(list.hasCity(city));
+    }
 }
